8abbedbc764a6c78b7570445eafd0968edf1f417
Caio Mathielo <mathielo@gmail.com>
2016-03-17 15:48:31 +0100

    Writing first part of plugin login (partial)

--- index.js
+++ index.js
@@ -3,12 +3,45 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-mediator', function (opts) {
     opts = opts || {};
 
-    // Work with options here
+    mediatorSettings = {}
+    mediatorQueries = {
+      'desktop': [
+        'div.myStyle{ width:100% }'
+      ]
+    }
 
     return function (css, result) {
-    	css.walkDecls(function (decl) {
-    		console.log(decl);
-    	});
-        // Transform CSS AST here
+    	css.walkAtRules(function (rule) {
+        if (rule.name == 'mediator') {
+          console.log(rule.params);
+
+          var firstSpace = rule.params.indexOf(' ')
+          var key = rule.params.substr(0, firstSpace)
+          var value = rule.params.substr(firstSpace + 1)
+          mediatorSettings[key] = value
+        }
+      });
+
+      css.walkDecls(function(decl, i){
+        // console.log(decl.prop)
+        // console.log(decl.value)
+        // console.log(decl)
+
+        if (decl.prop.indexOf('.') == -1) {
+          return;
+        }
+
+        var artifacts = decl.prop.split('.')
+        var prop = artifacts.pop()
+
+        console.log(decl)
+        console.log(prop)
+        console.log(artifacts)
+
+
+      });
+
+
+
     };
 });
