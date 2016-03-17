a07e21617e474f710ea24a74d540c9cb58b9a844
unknown <admin.henyah@gmail.com>
2016-03-17 10:22:16 +0100

    Initial stuff.

---
+++ index.js
@@ -0,0 +1,10 @@
+var postcss = require("postcss")
+
+/**
+ * PostCSS plugin - mediator
+ */
+module.exports = postcss.plugin("postcss-mediator", function() {
+  return function(style) {
+    
+  }
+})
